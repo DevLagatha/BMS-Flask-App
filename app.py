0b392e39b5479f2a29b37bdc6a76def9e66d646d
Agatha <agathajelagat97@gmail.com>
2025-11-16 23:42:53 +0300

    fix indentation

--- app.py
+++ app.py
@@ -179,5 +179,5 @@ if __name__ == '__main__':
         
         # (sample data stays as-is)
         
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=False)
 
